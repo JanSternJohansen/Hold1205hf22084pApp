8244638b015e6036e509c489a89e8daa494cc2de
JanSternJohansen <jaj@tec.dk>
2022-08-11 08:14:14 +0200

    Torsdag morgen

--- App1/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ App1/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.twoactivities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button btnGoToSecond;
+    EditText edtWriteToSecond;
+    TextView txtFromSecond;
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtFromSecond = findViewById(R.id.txtFromSecond);
+        edtWriteToSecond = findViewById(R.id.edtWriteToSecond);
+        btnGoToSecond = findViewById(R.id.btnGoToSecond);
+        btnGoToSecond.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("MessageToSecond", edtWriteToSecond.getText().toString());
+                //startActivity(intent);
+                startActivityForResult(intent, 1);
+                //someActivityResultLauncher.launch(intent);
+            }
+        });
+
+//       someActivityResultLauncher = registerForActivityResult(
+//                new ActivityResultContracts.StartActivityForResult(),
+//                new ActivityResultCallback<ActivityResult>() {
+//                    @Override
+//                    public void onActivityResult(ActivityResult result) {
+//                        if (result.getResultCode() == Activity.RESULT_OK) {
+//                            // There are no request codes
+//                            Intent data = result.getData();
+//                            String message = data.getStringExtra("FromSecond");
+//                            txtFromSecond.setText(message);
+//                        }
+//                    }
+  //              });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK)
+        {
+            String message = data.getStringExtra("FromSecond");
+            txtFromSecond.setText(message);
+        }
     }
 }
--- App1/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ App1/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -2,13 +2,37 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity
+{
+    TextView txtShowMessage;
+    EditText edtWriteReturnText;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        edtWriteReturnText = findViewById(R.id.edtWriteReturnText);
+
+        txtShowMessage = findViewById(R.id.txtSecond_ShowMessage);
+        intent = getIntent();
+        String message = intent.getStringExtra("MessageToSecond");
+        txtShowMessage.setText(message);
+    }
+
+    public void goBack(View v)
+    {
+        intent.putExtra("FromSecond", edtWriteReturnText.getText().toString());
+        setResult(Activity.RESULT_OK, intent);
+        finish();
     }
 }
